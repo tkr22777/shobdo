331a6043c517cdac0b06e879e786c075bd86a447
Tahsin Kabir <tahsin@cs.unc.edu>
2016-05-28 01:18:47 -0600

    Added Play Java application barebone

---
+++ application/dict-app/app/controllers/WordController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import scala.Int;
+
+/**
+ * Created by tahsinkabir on 5/28/16.
+ */
+public class WordController extends Controller{
+
+    public Result index(){
+        return ok("বাংলা অভিধান এ স্বাগতম!" );
+    }
+
+    public Result length(String word, int length){
+        int s_size = word.length();
+        int len = Int.unbox(length);
+
+        if(s_size == len)
+            return ok("Yay! You made the correct character count of the word: " + word );
+        if(s_size > len)
+            return ok("Ups, the length of " + word + " is bigger than you thought!" );
+        else
+            return ok("Ups, the length of " + word + " is smaller than you thought!" );
+    }
+}
