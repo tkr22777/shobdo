43a26f6aff2f397ae22bf9036022bf419bc41d78
Tahsin Kabir <tahsin@cs.unc.edu>
2017-01-21 14:55:36 -0700

    Added test_local file. Renamed test_local.sh back to test :P

--- scripts/curl_api_test_commands.sh
+++ scripts/curl_api_test_commands.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
 #Splash screen
-curl -X GET http://192.168.99.100:9000/dict
+IP="192.168.99.100"
+PORT="9000"
+echo "curl sanity tests:"
+
+echo "Testing GET home route:"
+curl -X GET http://$IP:$PORT/dict
 echo ""
 
-#Testing post route
-curl -H "Content-Type: application/json" -X POST -d '{"name":"SIN"}' http://192.168.99.100:9000/posttest
+#Testing POST route
+echo "Testing POST route:"
+curl -H "Content-Type: application/json" -X POST -d '{"name":"SIN"}' http://$IP:$PORT/posttest
 echo ""
 
 #Generating a test dictionary
-curl -H "Content-Type: application/json" -X POST -d '{"wordCount":"100"}' http://192.168.99.100:9000/dict/generate
+echo "Testing Temp Dictionary Generate:"
+curl -H "Content-Type: application/json" -X POST -d '{"wordCount":"100"}' http://$IP:$PORT/dict/generate
 echo ""
 
 #Serch word by spelling in the dictionary
-curl -H "Content-Type: application/json" -X POST -d '{"spelling":"ঙ"}' http://192.168.99.100:9000/dict/search
+echo "Testing Search POST:"
+curl -H "Content-Type: application/json" -X POST -d '{"spelling":"ঙ"}' http://$IP:$PORT/dict/search
 echo ""
 
 #Get word by spelling in the dictionary
-curl -H "Content-Type: application/json" -X POST -d '{"spelling":"ঙ"}' http://192.168.99.100:9000/dict/word
-echo ""
+#echo "Testing get word POST:"
+#curl -H "Content-Type: application/json" -X POST -d '{"spelling":"ঙ"}' http://192.168.99.100:$PORT/dict/word
+#echo ""
