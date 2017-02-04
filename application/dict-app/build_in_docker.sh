524a1840fdb04cd2befbe2fb498399f9b15169b7
Tahsin Kabir <tahsink@qualtrics.com>
2017-02-03 18:04:18 -0700

    Refactored some container naming

--- application/dict-app/build_in_docker.sh
+++ application/dict-app/build_in_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Removing previous image
-docker rm -f play_build_instance
+docker rm -f centos-java8-activator_instance
 
 #Building the app binaries for distribution
-docker run -v $PWD:/dict-app/code --name play_build_instance -it play_build /dict-app/code/build_activator_dist_local.sh
+docker run -v $PWD:/dict-app/code --name centos-java8-activator_instance -it stupefied/centos-java8-activator /dict-app/code/build_local.sh
--- application/dict-app/docker/01_centos_java8_activator/build_centos_java_activator.sh
+++ application/dict-app/docker/01_centos_java8_activator/build_centos_java_activator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #This builds the image that can build the play framework app
-docker build -t centos_java_activator .
+docker build -t centos-java8-activator .
 
