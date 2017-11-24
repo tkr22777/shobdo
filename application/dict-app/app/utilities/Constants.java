47f64c90f761a95ce5903c51ecffbf7b38b5d11b
Tahsin Kabir <tahsinkabir@gmail.com>
2017-11-23 21:45:14 -0800

    Refactored constant names

--- application/dict-app/app/utilities/Constants.java
+++ application/dict-app/app/utilities/Constants.java
@@ -5,12 +5,12 @@ package utilities;
  */
 public class Constants {
 
-    public static final String MONGO_DB_HOSTNAME_CONFIG_STRING = "shobdo.mongodbhostname";
-    public static final String MONGO_DB_PORT_CONFIG_STRING = "shobdo.mongodbport";
-    public static final String REDIS_HOSTNAME_CONFIG_STRING = "shobdo.redishostname";
-
+    public static final String MONGODB_HOSTNAME_CONFIG_STRING = "shobdo.mongodbhostname";
+    public static final String MONGODB_PORT_CONFIG_STRING = "shobdo.mongodbport";
     public static final String DICTIONARY_DATABASE_NAME = "Dictionary";
     public static final String WORD_COLLECTION_NAME = "Words";
+
+    public static final String REDIS_HOSTNAME_CONFIG_STRING = "shobdo.redishostname";
 
     public static final int SEARCH_SPELLING_LIMIT = 40;
 
