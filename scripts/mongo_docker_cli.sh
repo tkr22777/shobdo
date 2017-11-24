5a277f52ab4357042486673dc07d32ec12b1ffa8
Tahsin Kabir <tahsinkabir@gmail.com>
2017-11-23 17:10:55 -0800

    Adde dumb scripts to access mongo and redis cli because I am lazy

---
+++ scripts/mongo_docker_cli.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Entering mongo docker instances' command prompt 
+docker exec -it mongo-instance mongo
---
+++ scripts/redis_docker_cli_raw.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Entering redis docker instances' command prompt 
+docker exec -it redis-instance redis-cli --raw
