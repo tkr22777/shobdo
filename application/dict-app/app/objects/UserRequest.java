c114e38e16784ac02a975f4ed05e4ec66e2c7a8c
Tahsin Kabir <tahsinkabir@gmail.com>
2018-02-10 08:42:32 -0800

    Some more refactors

--- application/dict-app/app/objects/UserRequest.java
+++ application/dict-app/app/objects/UserRequest.java
@@ -2,9 +2,10 @@ package objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
+@Data @AllArgsConstructor
 public class UserRequest {
 
     @JsonIgnore
